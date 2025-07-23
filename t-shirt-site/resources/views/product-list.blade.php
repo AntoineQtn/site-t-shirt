4dccdca5719952a0984f8e5a7cd3dcc02889e89d
Antoine QUINTIN <antoine.quintin@le-campus-numerique.fr>
2025-07-23 16:01:48 +0200

    init

---
+++ t-shirt-site/resources/views/product-list.blade.php
@@ -0,0 +1,20 @@
+<div class="advertising">
+    <p>TOUJOURS PLUS DE SOLDES!</p>
+</div>
+<h1>Titre</h1>
+<a href="{{route('product')}}">
+    <img src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dCUyMHNoaXJ0JTIwYmxhbmN8ZW58MHx8MHx8fDA%3D" alt="t-shirt-image">
+</a>
+
+
+<style>
+
+    img {
+        width: 10%;
+        height:  auto;
+    }
+    button{
+        background-color: red;
+        color: white;
+    }
+</style>
--- t-shirt-site/routes/web.php
+++ t-shirt-site/routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\ProductListController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/product-list', [ProductListController::class, 'show']);
+Route::get('/cart', [CartController::class, 'show']);
 
 Route::get('/product', [ProductController::class, 'index']); 
