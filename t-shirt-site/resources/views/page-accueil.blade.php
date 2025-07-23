46eacea465108c0b60424dc64932beb06bccdb57
abdoulaye soumare <abdoulaye.soumare@le-campus-numerique.fr>
2025-07-23 15:27:26 +0200

    initiation

---
+++ t-shirt-site/resources/views/page-accueil.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
+</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page d'accueil</title>
+</head>
+<body>
+    <header>
+        page d'accueil
+    </header>
+</body>
+</html>
